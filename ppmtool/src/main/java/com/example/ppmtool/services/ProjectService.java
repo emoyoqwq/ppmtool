c7f2cbc09e6be11877bf53c9394bfe9c927674a7
emoyo <emoyoqaq@gmail.com>
2024-08-23 10:13:10 -0400

    add CRUD operation

--- ppmtool/src/main/java/com/example/ppmtool/services/ProjectService.java
+++ ppmtool/src/main/java/com/example/ppmtool/services/ProjectService.java
@@ -1,5 +1,6 @@
 package com.example.ppmtool.services;
 
+import com.example.ppmtool.Exceptions.ProjectIdException;
 import com.example.ppmtool.domain.Project;
 import com.example.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +13,35 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdate(Project project) {
-
-        return projectRepository.save(project);
+        try{
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        }catch(Exception e){
+            throw new ProjectIdException("Project ID " + project.getProjectIdentifier().toUpperCase()+ " already exists");
+        }
     }
+
+    public Project findProjectByProjectIdentifier(String projectIdentifier) {
+        Project project = projectRepository.findByProjectIdentifier(projectIdentifier.toUpperCase());
+
+        if(project == null){
+            throw new ProjectIdException("Project ID " + projectIdentifier.toUpperCase() + " does not exist");
+        }
+        return project;
+    }
+
+    public Iterable<Project> findAllProjects() {
+        return projectRepository.findAll();
+    }
+
+    public void deleteProjectByIdentifier(String projectIdentifier) {
+        Project project = projectRepository.findByProjectIdentifier(projectIdentifier.toUpperCase());
+        if(project == null){
+            throw new ProjectIdException("cannot delete project with ID ' " + projectIdentifier.toUpperCase() + " ' This project does not exist");
+        }
+
+        projectRepository.delete(project);
+    }
+
+
 }
--- ppmtool/src/main/java/com/example/ppmtool/web/ProjectController.java
+++ ppmtool/src/main/java/com/example/ppmtool/web/ProjectController.java
@@ -10,10 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
@@ -35,7 +32,25 @@ public class ProjectController {
         ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
         if(errorMap != null) return errorMap;
 
-        Project project1 = projectService.saveOrUpdate(project);
+        projectService.saveOrUpdate(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
     }
+
+    @GetMapping("/{projectId}")
+    public ResponseEntity<?> getProjectById(@PathVariable String projectId) {
+        Project project = projectService.findProjectByProjectIdentifier(projectId);
+        return new ResponseEntity<Project>(project, HttpStatus.OK);
+    }
+
+    @GetMapping("/all")
+    public Iterable<Project> getAllProjects() {
+        return projectService.findAllProjects();
+    }
+
+    @DeleteMapping("/{projectId}")
+    public ResponseEntity<?> deleteProject(@PathVariable String projectId) {
+        projectService.deleteProjectByIdentifier(projectId);
+        return new ResponseEntity<String>("Project with ID: " + projectId + " was deleted",HttpStatus.OK);
+    }
+    
 }
