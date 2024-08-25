823c28fb77650df611e9d88d51722dd3c86945ed
emoyo <emoyoqaq@gmail.com>
2024-08-25 09:55:26 -0400

    frontend lec12

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -5,22 +5,24 @@ import Header from "./components/Layout/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import AddProject from './components/Project/AddProject';
+import { Provider } from 'react-redux';
+import store from "./store";
 
 
 class App extends Component {
   render() {
     return (
-      <Router>
-      
-        <div className="App">
-          <Header />
-          <Routes>
-          <Route path="/dashBoard" element = {<Dashboard />} />
-          <Route path="/addProject" element = {<AddProject />} />
-          </Routes>
-        </div>
-        
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Header />
+            <Routes>
+              <Route path="/dashBoard" element = {<Dashboard />} />
+              <Route path="/addProject" element = {<AddProject />} />
+            </Routes>
+          </div>
+        </Router>
+      </Provider>
     );
     
   }
