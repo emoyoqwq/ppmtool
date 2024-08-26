6579beb2f6d0c645dc360d018b65fec6a8e66a21
emoyo <emoyoqaq@gmail.com>
2024-08-26 04:55:10 -0400

    frontend lec19

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -7,6 +7,7 @@ import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import AddProject from './components/Project/AddProject';
 import { Provider } from 'react-redux';
 import store from "./store";
+import UpdateProject from './components/Project/UpdateProject';
 
 
 class App extends Component {
@@ -17,8 +18,9 @@ class App extends Component {
           <div className="App">
             <Header />
               <Routes>
-                <Route path="/dashBoard" element = {<Dashboard />} />
-                <Route path="/addProject" element = {<AddProject />} />
+                <Route exact path="/dashBoard" element = {<Dashboard />} />
+                <Route exact path="/addProject" element = {<AddProject />} />
+                <Route exact path="/updateProject/:id" element={<UpdateProject />} />
               </Routes>
           </div>
         </Router>
